9760ee55aeb1fecdb853324b749a9a918e86721d
Yuhan Zhang <zyh514312@gmail.com>
2022-05-16 17:35:19 +0000

    JS primitives and variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+var fullName = 'Yuhan Zhang';
+var isCool = true;
+var totalPets = 1;
+
+console.log('value of fullName: ', fullName);
+console.log('value of isCool: ', isCool);
+console.log('value of totalPets: ', totalPets);
+console.log('typeof fullName: ', typeof 'Yuhan Zhang');
+console.log('typeof isCool: ', typeof true);
+console.log('typeof totalPets: ', typeof 1);
+
+var unknown = null;
+console.log('value of unknown: ', unknown);
+console.log('type of unknown: ', typeof null);
+
+var never;
+console.log('value of never: ', never);
+console.log('type of never:', typeof never);
