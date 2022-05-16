bff6e225e9b0adc2eaa167815acbbd0e8a553a79
Yuhan Zhang <zyh514312@gmail.com>
2022-05-16 21:17:31 +0000

    JS operators and expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 3;
+var height = 4;
+var area = width * height;
+console.log('value of area: ', area);
+console.log('type of area: ', typeof area);
+
+var bill = 19;
+var payment = 20;
+var change = bill - payment;
+console.log('value of change: ', change);
+console.log('type of change:', typeof change);
+
+var quizzes = 90;
+var midterm = 95;
+var final = 100;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ', grade);
+console.log('type of grade:', typeof grade);
+
+var firstName = 'Peter';
+var lastName = 'Parker';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName: ', fullName);
+console.log('type of fullName:', typeof fullName);
+
+var pH = 8;
+var isAcidic = pH < 7;
+console.log('value of isAcidic: ', isAcidic);
+console.log('type of isAcidic:', typeof isAcidic);
+
+var headCount = 97;
+var isSparta = headCount === 300;
+console.log('value of isSparta: ', isSparta);
+console.log('type of isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('value of motto: ', motto);
+console.log('type of motto:', typeof motto);
