dfeddd96b7a18b591a2fb3e466df21d2cbe9a727
Yuhan Zhang <zyh514312@gmail.com>
2022-05-17 18:56:51 +0000

    naming correction

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,65 +1,65 @@
 var orderHistory = [
   {
-    order_number: '114-3941689-8772232',
-    order_placed: 'August 4, 2020',
+    orderNumber: '114-3941689-8772232',
+    orderPlaced: 'August 4, 2020',
     total: '$34.00',
-    ship_to: 'JS Masher',
-    delivered_date: 'Aug 8, 2020',
-    order_detail: [{
+    shipTo: 'JS Masher',
+    deliveredDate: 'Aug 8, 2020',
+    orderDetails: [{
       name: 'JavaScript for impatient programmers',
       author: 'Rauschmayer, Dr. Axel',
-      Return_closed: 'Sep 7, 2020',
+      returnClosed: 'Sep 7, 2020',
       price: '$31.55'
     }
     ]
   },
   {
-    order_number: '113-9984268-1280257',
-    order_placed: 'July 19, 2020',
+    orderNumber: '113-9984268-1280257',
+    orderPlaced: 'July 19, 2020',
     total: '$44.53',
-    ship_to: 'JS Masher',
-    delivered_date: 'Jul 20, 2020',
-    order_detail: [{
+    shipTo: 'JS Masher',
+    deliveredDate: 'Jul 20, 2020',
+    orderDetails: [{
       name: 'The Timeless Way of Building',
       author: 'Alexander, Christopher',
-      Return_closed: 'Aug 19, 2020',
+      returnClosed: 'Aug 19, 2020',
       price: '$41.33'
     }
     ]
   },
   {
-    order_number: '114-2875557-9059409',
-    order_placed: 'July 4, 2020',
+    orderNumber: '114-2875557-9059409',
+    orderPlaced: 'July 4, 2020',
     total: '$17.22',
-    ship_to: 'JS Masher',
-    delivered_date: 'Jul 7, 2020',
-    order_detail: [{
+    shipTo: 'JS Masher',
+    deliveredDate: 'Jul 7, 2020',
+    orderDetails: [{
       name: 'Gamecube Controller Adapter.Super Smash Bros Switch Gamecube Adapter for WII U,PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-      Return_closed: 'Aug 5, 2020',
+      returnClosed: 'Aug 5, 2020',
       price: '$15.98'
     }
     ]
   },
   {
-    order_number: '113-2883177-2648248',
-    order_placed: 'July 3, 2020',
+    orderNumber: '113-2883177-2648248',
+    orderPlaced: 'July 3, 2020',
     total: '$138.93',
-    ship_to: 'JS Masher',
-    delivered_date: 'Jul 5, 2020',
-    order_detail: [
+    shipTo: 'JS Masher',
+    deliveredDate: 'Jul 5, 2020',
+    orderDetails: [
       {
         name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-        Return_closed: 'Aug 4, 2020',
+        returnClosed: 'Aug 4, 2020',
         price: '$94.95'
       },
       {
         name: 'The Art of Sql',
         author: 'Faroult, Stephane',
-        Return_closed: 'Aug 4, 2020',
+        returnClosed: 'Aug 4, 2020',
         price: '$33.99'
       }
     ]
   }
 ];
 
-console.log('The name of fourth order second item: ', orderHistory[3].order_detail[1].name);
+console.log('The name of fourth order second item: ', orderHistory[3].orderDetails[1].name);
