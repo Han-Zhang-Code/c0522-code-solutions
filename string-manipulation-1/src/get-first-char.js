1a9e53d541e7fb0b6f22830ce551fb171192284c
Yuhan Zhang <zyh514312@gmail.com>
2022-05-20 23:45:32 +0000

    correction

--- string-manipulation-1/src/get-first-char.js
+++ string-manipulation-1/src/get-first-char.js
@@ -1,14 +1,7 @@
 /* exported getFirstChar */
 /*
-declar a new empty string variable
-use for loop to iterate the string
-in the for loop, assign the first index of the string array to the new empty string variable
-after the for loop return to the new string variable
+return the first index of the string
 */
 function getFirstChar(string) {
-  var char = '';
-  for (var i = 0; i < string.length; i++) {
-    char = string[0];
-  }
-  return char;
+  return string[0];
 }
--- string-manipulation-1/src/get-last-char.js
+++ string-manipulation-1/src/get-last-char.js
@@ -1,14 +1,7 @@
 /* exported getLastChar */
 /*
-declar a new empty string variable
-use for loop to iterate the string
-in the for loop, assign the last index of the string array to the new empty string variable
-after the for loop return to the new string variable
+return the last index of the string
 */
 function getLastChar(string) {
-  var char = '';
-  for (var i = 0; i < string.length; i++) {
-    char = string[string.length - 1];
-  }
-  return char;
+  return string[string.length - 1];
 }
