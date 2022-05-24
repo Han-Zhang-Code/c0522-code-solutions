73e65f331b62f60e16f873f430e518fcce3ae822
Yuhan Zhang <zyh514312@gmail.com>
2022-05-24 23:24:03 +0000

    correction

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,13 +1,18 @@
 var $lightbulb = document.querySelector('.circle-night');
 var $room = document.querySelector('.background-night');
+
+var ifClicked = true;
 function clicked() {
-  if ($lightbulb.className === 'circle-night') {
+
+  if (ifClicked) {
 
     $lightbulb.className = 'circle-day';
     $room.className = 'background-day';
-  } else if ($lightbulb.className === 'circle-day') {
+    ifClicked = false;
+  } else if (!ifClicked) {
     $lightbulb.className = 'circle-night';
     $room.className = 'background-night';
+    ifClicked = true;
   }
 }
 $lightbulb.addEventListener('click', clicked);
