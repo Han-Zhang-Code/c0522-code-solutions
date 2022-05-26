9db2472e7c5d3b4c02a33e5161d7dbba904d5a83
Yuhan Zhang <zyh514312@gmail.com>
2022-05-26 18:20:53 +0000

    not done

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -4,13 +4,21 @@ convert the object to array using Object.entries method
 swap the each array index
 convert the resulting array to object using Object.fromEntries method
 */
+// function invert(source) {
+//   var newArray = Object.entries(source);
+//   for (var i = 0; i < newArray.length; i++) {
+//     var temp = newArray[i][0];
+//     newArray[i][0] = newArray[i][1];
+//     newArray[i][1] = temp;
+//   }
+//   var newObject = Object.fromEntries(newArray);
+//   return newObject;
+// }
 function invert(source) {
-  var newArray = Object.entries(source);
-  for (var i = 0; i < newArray.length; i++) {
-    var temp = newArray[i][0];
-    newArray[i][0] = newArray[i][1];
-    newArray[i][1] = temp;
+  var newObject = {};
+  for (var keys in source) {
+    newObject[keys] = keys;
+    // keys = source[keys];
   }
-  var newObject = Object.fromEntries(newArray);
   return newObject;
 }
