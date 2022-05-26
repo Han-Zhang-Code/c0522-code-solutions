aa5c7a14e410c79c7313b98c1862e3131f06424c
Yuhan Zhang <zyh514312@gmail.com>
2022-05-26 03:23:15 +0000

    not done

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -3,9 +3,10 @@ function keyIn(event) {
   var $keyIn = document.querySelectorAll('span');
   if (event.key === $keyIn[currentIndex].outerText || (event.key === ' ' && $keyIn[currentIndex].innerHTML === '&nbsp;')) {
     $keyIn[currentIndex].className = 'make-green';
+    $keyIn[currentIndex + 1].className = 'add-underline';
     currentIndex++;
   } else {
-    $keyIn[currentIndex].className = 'make-red';
+    $keyIn[currentIndex].className = 'make-red add-underline';
   }
 }
 document.addEventListener('keydown', keyIn);
