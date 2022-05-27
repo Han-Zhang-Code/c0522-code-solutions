0f91cb0fda3be23402c34a8260be5efe2aa28c9c
Yuhan Zhang <zyh514312@gmail.com>
2022-05-27 22:25:59 +0000

    array manipulation 3 done

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,24 @@
 /* exported difference */
+/*
+declare an empty new array
+use for loop to iterate through first array, if the value at first array index
+is not found at the second array, push the variable to the new array
+do the same thing for the second array and compare to first array
+return to the new array variable
+*/
+
+function difference(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (!second.includes(first[i])) {
+      newArray.push(first[i]);
+    }
+  }
+  for (var j = 0; j < second.length; j++) {
+    if (!first.includes(second[j])) {
+      newArray.push(second[j]);
+    }
+  }
+  return newArray;
+
+}
