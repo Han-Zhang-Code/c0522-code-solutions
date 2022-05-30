fc516a4257c1ee8f860309a806bbce34e9e6ab1d
Yuhan Zhang <zyh514312@gmail.com>
2022-05-30 17:02:30 +0000

    half done

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,29 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+var players = [{
+  name: 'Han',
+  hand: []
+}, {
+  name: 'James',
+  hand: []
+}, {
+  name: 'Tim',
+  hand: []
+}, {
+  name: 'Nathan',
+  hand: []
+}];
+
+var suit = ['clubs', 'diamonds', 'hearts', 'spades'];
+var rank = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+var cardDeck = [];
+for (var i = 0; i < suit.length; i++) {
+  for (var j = 0; j < rank.length; j++) {
+    var card = { suit: suit[i], rank: rank[j] };
+    cardDeck.push(card);
+  }
+}
+console.log(cardDeck);
+var shuffled = _.shuffle(cardDeck);
+console.log(shuffled);
+console.log(_.shuffle([1, 2, 3, 4]));
+console.log(players);
