10cf3d1de93fa1cdc03b62f15855fabf3288e4c7
Yuhan Zhang <zyh514312@gmail.com>
2022-06-09 06:41:46 +0000

    javascript-carousel dynamic version done

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,120 +1,67 @@
-var $img = document.querySelector('#image');
-var $dots1 = document.querySelector('#bulbasaur');
-$dots1.addEventListener('click', selectedBulbasaur);
-function selectedBulbasaur() {
+var $circles = document.querySelector('.circles');
+var $circle = document.querySelectorAll('.circle');
+var $views = document.querySelectorAll('.view');
+$circles.addEventListener('click', selected);
+function selected(event) {
   clearInterval(intervalID);
   intervalID = setInterval(carousel, 3000);
-  $img.setAttribute('src', 'images/001.png');
-  $img.setAttribute('alt', '001.png');
-  $dots1.className = 'fas fa-circle add-padding-5';
-  $dots2.className = 'far fa-circle add-padding-5';
-  $dots3.className = 'far fa-circle add-padding-5';
-  $dots4.className = 'far fa-circle add-padding-5';
-  $dots5.className = 'far fa-circle add-padding-5';
+  var $dataview = event.target.getAttribute('data-view');
+  for (var i = 0; i < $circle.length; i++) {
+    if ($circle[i].getAttribute('data-view') === $dataview) {
+      $circle[i].className = 'fas fa-circle add-padding-5 circle';
+      $views[i].className = 'view';
+    } else if ($circle[i].getAttribute('data-view') !== $dataview) {
+      $circle[i].className = 'far fa-circle add-padding-5 circle';
+      $views[i].className = 'view hidden';
+    }
+  }
 }
-var $dots2 = document.querySelector('#charmander');
-$dots2.addEventListener('click', selectedCharmander);
-function selectedCharmander() {
-  clearInterval(intervalID);
-  intervalID = setInterval(carousel, 3000);
-  $img.setAttribute('src', 'images/004.png');
-  $img.setAttribute('alt', '004.png');
-  $dots1.className = 'far fa-circle add-padding-5';
-  $dots2.className = 'fas fa-circle add-padding-5';
-  $dots3.className = 'far fa-circle add-padding-5';
-  $dots4.className = 'far fa-circle add-padding-5';
-  $dots5.className = 'far fa-circle add-padding-5';
-}
-
-var $dots3 = document.querySelector('#squirtle');
-$dots3.addEventListener('click', selectedSquirtle);
-function selectedSquirtle() {
-  clearInterval(intervalID);
-  intervalID = setInterval(carousel, 3000);
-  $img.setAttribute('src', 'images/007.png');
-  $img.setAttribute('alt', '007.png');
-  $dots1.className = 'far fa-circle add-padding-5';
-  $dots2.className = 'far fa-circle add-padding-5';
-  $dots3.className = 'fas fa-circle add-padding-5';
-  $dots4.className = 'far fa-circle add-padding-5';
-  $dots5.className = 'far fa-circle add-padding-5';
-}
-
-var $dots4 = document.querySelector('#pikachu');
-$dots4.addEventListener('click', selectedPikachu);
-function selectedPikachu() {
-  clearInterval(intervalID);
-  intervalID = setInterval(carousel, 3000);
-  $img.setAttribute('src', 'images/025.png');
-  $img.setAttribute('alt', '025.png');
-  $dots1.className = 'far fa-circle add-padding-5';
-  $dots2.className = 'far fa-circle add-padding-5';
-  $dots3.className = 'far fa-circle add-padding-5';
-  $dots4.className = 'fas fa-circle add-padding-5';
-  $dots5.className = 'far fa-circle add-padding-5';
-}
-
-var $dots5 = document.querySelector('#jigglypuff');
-$dots5.addEventListener('click', selectedJigglypuff);
-function selectedJigglypuff() {
-  clearInterval(intervalID);
-  intervalID = setInterval(carousel, 3000);
-  $img.setAttribute('src', 'images/039.png');
-  $img.setAttribute('alt', '039.png');
-  $dots1.className = 'far fa-circle add-padding-5';
-  $dots2.className = 'far fa-circle add-padding-5';
-  $dots3.className = 'far fa-circle add-padding-5';
-  $dots4.className = 'far fa-circle add-padding-5';
-  $dots5.className = 'fas fa-circle add-padding-5';
-}
-
 var $arrowRight = document.querySelector('.fa-chevron-right');
-$arrowRight.addEventListener('click', nextPic);
-function nextPic() {
+$arrowRight.addEventListener('click', arrowActivityRight);
+var count = 0;
+function arrowActivityRight(event) {
   clearInterval(intervalID);
   intervalID = setInterval(carousel, 3000);
-  if ($img.getAttribute('alt') === '001.png') {
-    selectedCharmander();
-  } else if ($img.getAttribute('alt') === '004.png') {
-    selectedSquirtle();
-  } else if ($img.getAttribute('alt') === '007.png') {
-    selectedPikachu();
-  } else if ($img.getAttribute('alt') === '025.png') {
-    selectedJigglypuff();
-  } else if ($img.getAttribute('alt') === '039.png') {
-    selectedBulbasaur();
+  if (count < $views.length - 1) {
+    $circle[count].className = 'far fa-circle add-padding-5 circle';
+    $views[count].className = 'view hidden';
+    $circle[count + 1].className = 'fas fa-circle add-padding-5 circle';
+    $views[count + 1].className = 'view';
+  } else if (count === $views.length - 1) {
+    $circle[count].className = 'far fa-circle add-padding-5 circle';
+    $views[count].className = 'view hidden';
+    $circle[0].className = 'fas fa-circle add-padding-5 circle';
+    $views[0].className = 'view';
+  }
+  if (count < $views.length - 1) {
+    count++;
+  } else if (count === $views.length - 1) {
+    count = 0;
   }
 }
-
 var $arrowLeft = document.querySelector('.fa-chevron-left');
-$arrowLeft.addEventListener('click', lastPic);
-function lastPic() {
+$arrowLeft.addEventListener('click', arrowActivityLeft);
+function arrowActivityLeft(event) {
   clearInterval(intervalID);
   intervalID = setInterval(carousel, 3000);
-  if ($img.getAttribute('alt') === '001.png') {
-    selectedJigglypuff();
-  } else if ($img.getAttribute('alt') === '039.png') {
-    selectedPikachu();
-  } else if ($img.getAttribute('alt') === '025.png') {
-    selectedSquirtle();
-  } else if ($img.getAttribute('alt') === '007.png') {
-    selectedCharmander();
-  } else if ($img.getAttribute('alt') === '004.png') {
-    selectedBulbasaur();
+  if (count > 0) {
+    $circle[count].className = 'far fa-circle add-padding-5 circle';
+    $views[count].className = 'view hidden';
+    $circle[count - 1].className = 'fas fa-circle add-padding-5 circle';
+    $views[count - 1].className = 'view';
+  } else if (count === 0) {
+    $circle[count].className = 'far fa-circle add-padding-5 circle';
+    $views[count].className = 'view hidden';
+    $circle[$views.length - 1].className = 'fas fa-circle add-padding-5 circle';
+    $views[$views.length - 1].className = 'view';
+  }
+  if (count > 0) {
+    count--;
+  } else if (count === 0) {
+    count = $views.length - 1;
   }
 }
-
 var intervalID = setInterval(carousel, 3000);
 function carousel() {
-  if ($img.getAttribute('alt') === '001.png') {
-    selectedCharmander();
-  } else if ($img.getAttribute('alt') === '004.png') {
-    selectedSquirtle();
-  } else if ($img.getAttribute('alt') === '007.png') {
-    selectedPikachu();
-  } else if ($img.getAttribute('alt') === '025.png') {
-    selectedJigglypuff();
-  } else if ($img.getAttribute('alt') === '039.png') {
-    selectedBulbasaur();
-  }
+  arrowActivityRight();
 }
