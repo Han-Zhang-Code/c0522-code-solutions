b93a25ab2fccb74efbf3f4e9e34ad0e95f807f46
Yuhan Zhang <zyh514312@gmail.com>
2022-06-30 22:56:27 +0000

    this challenge feels more about object manipulation rather than node.js

---
+++ node-notes-cli/app.js
@@ -0,0 +1,27 @@
+const notes = require('./data.json');
+const fs = require('fs');
+if (process.argv[2] === 'read') {
+  for (const k in notes.notes) {
+    console.log(k + ':' + notes.notes[k]);
+  }
+} else if (process.argv[2] === 'create') {
+  notes.notes[notes.nextId] = process.argv[3];
+  notes.nextId++;
+  var newNotes = JSON.stringify(notes, null, 2);
+  fs.writeFile('data.json', newNotes, err => {
+    if (err) throw err;
+  });
+} else if (process.argv[2] === 'delete') {
+  delete notes.notes[process.argv[3]];
+  notes.nextId--;
+  newNotes = JSON.stringify(notes, null, 2);
+  fs.writeFile('data.json', newNotes, err => {
+    if (err) throw err;
+  });
+} else if (process.argv[2] === 'update') {
+  notes.notes[process.argv[3]] = process.argv[4];
+  newNotes = JSON.stringify(notes, null, 2);
+  fs.writeFile('data.json', newNotes, err => {
+    if (err) throw err;
+  });
+}
