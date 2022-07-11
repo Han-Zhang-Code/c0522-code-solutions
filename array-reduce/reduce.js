578195212d94c4e59502b6a9fc8feffe8dfb0133
Yuhan Zhang <zyh514312@gmail.com>
2022-07-11 23:36:21 +0000

    initialization taking consideration

--- array-reduce/reduce.js
+++ array-reduce/reduce.js
@@ -1,12 +1,16 @@
 function reduce(array, reducer, initialValue) {
   if (initialValue !== undefined) {
     var previous = initialValue;
+    for (var i = 0; i < array.length; i++) {
+      previous = reducer(previous, array[i]);
+    }
   } else {
     previous = array[0];
+    for (var j = 1; j < array.length; j++) {
+      previous = reducer(previous, array[j]);
+    }
   }
-  for (var i = 0; i < array.length; i++) {
-    previous = reducer(previous, array[i]);
-  }
+
   return previous;
 }
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
