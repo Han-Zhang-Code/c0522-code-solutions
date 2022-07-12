c5e44c39f81390f092418afdc38c64ff5595ce8d
Yuhan Zhang <zyh514312@gmail.com>
2022-07-12 00:02:42 +0000

    small correction for cb function

--- array-reduce/reduce.js
+++ array-reduce/reduce.js
@@ -2,12 +2,12 @@ function reduce(array, reducer, initialValue) {
   if (initialValue !== undefined) {
     var previous = initialValue;
     for (var i = 0; i < array.length; i++) {
-      previous = reducer(previous, array[i]);
+      previous = reducer(previous, array[i], i, array);
     }
   } else {
     previous = array[0];
     for (var j = 1; j < array.length; j++) {
-      previous = reducer(previous, array[j]);
+      previous = reducer(previous, array[j], j, array);
     }
   }
 
