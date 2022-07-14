2e07e110ab831b83ac41fd1298949dce8ff0d971
Yuhan Zhang <zyh514312@gmail.com>
2022-07-14 23:19:32 +0000

    not done yet

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ToggleSwitch from './toggle-switch';
+
+const element = <ToggleSwitch />;
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(element);
