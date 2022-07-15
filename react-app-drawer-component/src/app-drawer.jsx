5817e9206c44314ec7d88223c3c283636f9450c6
Yuhan Zhang <zyh514312@gmail.com>
2022-07-15 16:05:51 +0000

    almost done

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -3,7 +3,36 @@ import React from 'react';
 class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { isOpen: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  render() {
+    let background = '';
+    let iconClass = 'fa-solid fa-bars font-size';
+    let menuContainer = 'menu-container hidden';
+    if (this.state.isOpen) {
+      background = 'background';
+      iconClass = 'hidden';
+      menuContainer = 'menu-container';
+    }
+    return (
+      <div onClick={this.handleClick} className={background}>
+        <div>
+          <i onClick={this.handleClick} className={iconClass}></i>
+        </div>
+        <div className={menuContainer}>
+          <ul className='row'>
+            <li className='list-head'>menu</li>
+            <li>about us</li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
