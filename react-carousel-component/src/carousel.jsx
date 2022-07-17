2102f74ddf04eeac3bb0e3ee01041209b27fb297
Yuhan Zhang <zyh514312@gmail.com>
2022-07-17 11:50:55 +0000

    ui done

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
-class Carousel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleClick = this.handleClick.bind(this);
-  }
+import React, { useState } from 'react';
 
-  handleClick() {
-    this.setState();
-  }
+const CarouPics = ({ index, isClicked, setIsClicked, data }) => {
+  return (
+    <div key={'container' + index}>
+      <img key={'image' + index} src={data} />
 
-  render() {
-    return (
-      <div><img src={this.props.images[0]} alt="" /></div>
-    );
-  }
+    </div>
+  );
+
+};
+
+function Carousel(props) {
+  const [isClicked, setIsClicked] = useState(0);
+
+  const icons = props.images.map((icon, index) => {
+    return <i key={index} className="fa-regular fa-circle" isClicked={isClicked} setIsClicked={setIsClicked}></i>;
+  });
+
+  const imagess = props.images.map((image, index) => {
+    return <CarouPics key={index} index={index} data={image} />;
+  });
+
+  return (
+    <div>
+      {imagess}
+
+   {icons}
+    </div>
+  );
 }
+
 export default Carousel;
