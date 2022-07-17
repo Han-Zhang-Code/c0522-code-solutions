539e574d473cd971f71a22375307ee8719116626
Yuhan Zhang <zyh514312@gmail.com>
2022-07-17 15:08:32 +0000

    I learned 70% of hook tonight lol

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,53 +1,73 @@
-import React, { useState } from 'react';
-
-let Arraylength = 0;
-const CarouPics = ({ index, isClicked, setIsClicked, data }) => {
-  return (
-    <div key={'container' + index}>
-      <img key={'image' + index} src={data} className={isClicked === index ? 'image' : 'image hidden'}/>
-    </div>
-  );
-};
-
-const LeftChevron = ({ index, isClicked, setIsClicked }) => {
-  const onClickFn = index => {
-    setIsClicked(prestate => {
-      prestate--;
-      if (prestate === -1) {
-        prestate = Arraylength - 1;
-      }
-      return prestate;
-    });
-  };
-  return <i className={isClicked === index ? 'fa-solid fa-chevron-left chevron-adjust' : 'hidden'} onClick={() => { onClickFn(index); }}></i>;
-};
-const RightChevron = ({ index, isClicked, setIsClicked }) => {
-  const onClickFn = index => {
-    setIsClicked(prestate => {
-      prestate++;
-      if (prestate === Arraylength) {
-        prestate = 0;
-      }
-      return prestate;
-    });
-  };
-  return <i className={isClicked === index ? 'fa-solid fa-chevron-right chevron-adjust' : 'hidden'} onClick={() => { onClickFn(index); }}></i>;
-};
+import React, { useState, useEffect } from 'react';
 
 function Carousel(props) {
   const [isClicked, setIsClicked] = useState(0);
-  Arraylength = props.images.length;
+  const Arraylength = props.images.length;
   const icons = props.images.map((icon, index) => {
     const onClickFn = index => {
       setIsClicked(prestate => {
-        if (prestate === index) {
-          return -1;
-        }
+        clearInterval(intervalID);
+        carousal();
         return index;
       });
     };
     return <i key={index} className={isClicked === index ? 'fa-solid fa-circle icon-adjust' : 'fa-regular fa-circle icon-adjust'} onClick={() => { onClickFn(index); }}></i>;
   });
+
+  let intervalID;
+  const carousal = () => {
+    intervalID = setInterval(() => {
+      setIsClicked(prestate => {
+        prestate++;
+        if (prestate === Arraylength) {
+          prestate = 0;
+        }
+        return prestate;
+      });
+    }, 2000);
+  };
+
+  useEffect(() => {
+    carousal();
+    return () => clearInterval(intervalID);
+  });
+  const CarouPics = ({ index, isClicked, setIsClicked, data }) => {
+    return (
+      <div key={'container' + index}>
+        <img key={'image' + index} src={data} className={isClicked === index ? 'image' : 'image hidden'} />
+      </div>
+    );
+  };
+
+  const LeftChevron = ({ index, isClicked, setIsClicked }) => {
+    const onClickFn = index => {
+      clearInterval(intervalID);
+      carousal();
+      setIsClicked(prestate => {
+        prestate--;
+        if (prestate === -1) {
+          prestate = Arraylength - 1;
+        }
+        return prestate;
+      });
+    };
+    return <i className={isClicked === index ? 'fa-solid fa-chevron-left chevron-adjust' : 'hidden'} onClick={() => { onClickFn(index); }}></i>;
+  };
+
+  const RightChevron = ({ index, isClicked, setIsClicked }) => {
+    const onClickFn = index => {
+      clearInterval(intervalID);
+      carousal();
+      setIsClicked(prestate => {
+        prestate++;
+        if (prestate === Arraylength) {
+          prestate = 0;
+        }
+        return prestate;
+      });
+    };
+    return <i className={isClicked === index ? 'fa-solid fa-chevron-right chevron-adjust' : 'hidden'} onClick={() => { onClickFn(index); }}></i>;
+  };
 
   const image = props.images.map((image, index) => {
     return <CarouPics key={index} index={index} data={image} isClicked={isClicked} setIsClicked={setIsClicked}/>;
