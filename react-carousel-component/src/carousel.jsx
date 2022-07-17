ca46ddf740e90a28e5aff0364ec15c47b0888dda
Yuhan Zhang <zyh514312@gmail.com>
2022-07-17 06:33:43 +0000

    setup

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState();
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+export default Carousel;
---
+++ react-carousel-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Carousel from './carousel';
+const element = <Carousel />;
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(element);
