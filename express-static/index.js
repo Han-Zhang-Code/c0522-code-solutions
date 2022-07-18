0177c00d0e88e373e1b1b74056ec30bf02a3bf6b
Yuhan Zhang <zyh514312@gmail.com>
2022-07-18 18:35:58 +0000

    loading picture

--- express-static/index.js
+++ express-static/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+
+app.get('/image1.jpg', (req, res) => {
+  const pathToImg = path.join(pathToPublic, 'image1.jpg');
+  fs.readFile(pathToImg, (err, data) => {
+    if (err) {
+      res.status(404).send('not found');
+    } else {
+      res.end(data);
+
+    }
+
+  });
+});
 
 const pathToPublic = path.join(__dirname, 'public');
 console.log(pathToPublic);
