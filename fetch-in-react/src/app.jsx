1bbaedb467951a936e2087fb08ff41812a3eab24
Yuhan Zhang <zyh514312@gmail.com>
2022-07-18 23:08:25 +0000

    not done

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import UserList from './user-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ users: data, isLoading: false });
+      });
+  }
+
+  render() {
+    return this.state.isLoading
+      ? <p>Loading...</p>
+      : <UserList users={this.state.users} />;
+  }
+}
+// function App() {
+//   const [user, setUser] = useState([]);
+//   const [loading, setloading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const res = await fetch(
+//         'https://jsonplaceholder.typicode.com/users',
+//       );
+//       const json = await res.json();
+//       setData(json.hits);
+//     };
+//     fetchData();
+//   }, [setData]);
+
+//   return (loading
+//     ? <p>Loading...</p>
+//     : <UserList users={user} />);
+// }
+
+// }
+
+export default App;
