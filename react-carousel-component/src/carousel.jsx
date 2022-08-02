caf0272072656c439b2fac281ab4078e6ae2c51e
Yuhan Zhang <zyh514312@gmail.com>
2022-08-02 02:04:24 +0000

    carousel now can pause

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -18,23 +18,28 @@ const RightChevron = ({ onClickFnRight }) => {
 
 function Carousel(props) {
   const [isClicked, setIsClicked] = useState(0);
-  let [intervalID] = useState();
+  const [pause, setPause] = useState(false);
   const Arraylength = props.images.length;
 
   const onClickFnIcon = index => {
+    setPause(true);
     setIsClicked(prestate => {
-      clearInterval(intervalID);
-      carousal();
       return index;
     });
   };
   const icons = props.images.map((icon, index) => {
     return <i key={index} className={isClicked === index ? 'fa-solid fa-circle icon-adjust' : 'fa-regular fa-circle icon-adjust'}
-    onClick={() => { onClickFnIcon(index); }}></i>;
+      onClick={() => { onClickFnIcon(index); }}></i>;
   });
 
-  const carousal = () => {
-    intervalID = setInterval(() => {
+  useEffect(() => {
+    setTimeout(() => { setPause(false); }, 3000);
+  });
+
+  useEffect(() => {
+    if (pause) return;
+
+    const timeoutId = setTimeout(() => {
       setIsClicked(prestate => {
         prestate++;
         if (prestate === Arraylength) {
@@ -42,36 +47,14 @@ function Carousel(props) {
         }
         return prestate;
       });
-    }, 2000);
-  };
-
-  useEffect(() => {
-    carousal();
-    return () => clearInterval(intervalID);
-  }, [isClicked]);
-  // the following comment out code will work as well after deleted all carousal() function
-  // useEffect(() => {
-  //
-  //   console.log(Date.now() - t, 'start timer');
-  //   const timeoutId = setTimeout(() => {
-  //     setIsClicked(prestate => {
-  //       prestate++;
-  //       if (prestate === Arraylength) {
-  //         prestate = 0;
-  //       }
-  //       return prestate;
-  //     });
-  //   }, 3000);
-  //   return () => {
-  //     console.log(Date.now() - t, 'clean up carousal', timeoutId);
-  //     // clearInterval(intervalID);
-  //     clearTimeout(timeoutId);
-  //   };
-  // },[isClicked]);
+    }, 3000);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  });
 
   const onClickFnLeft = () => {
-    clearInterval(intervalID);
-    carousal();
+    setPause(true);
     setIsClicked(prestate => {
       prestate--;
       if (prestate === -1) {
@@ -82,8 +65,7 @@ function Carousel(props) {
   };
 
   const onClickFnRight = () => {
-    clearInterval(intervalID);
-    carousal();
+    setPause(true);
     setIsClicked(prestate => {
       prestate++;
       if (prestate === Arraylength) {
@@ -94,18 +76,18 @@ function Carousel(props) {
   };
 
   const image = props.images.map((image, index) => {
-    return <CarouPics key={index} index={index} data={image} isClicked={isClicked} setIsClicked={setIsClicked}/>;
+    return <CarouPics key={index} index={index} data={image} isClicked={isClicked} setIsClicked={setIsClicked} />;
   });
 
   return (
     <div className='container'>
       <div className='chevron'>
-        <LeftChevron onClickFnLeft={onClickFnLeft}/>
-       {image}
-        <RightChevron onClickFnRight={onClickFnRight}/>
+        <LeftChevron onClickFnLeft={onClickFnLeft} />
+        {image}
+        <RightChevron onClickFnRight={onClickFnRight} />
       </div>
       <div className='icon-container'>
-       {icons}
+        {icons}
       </div>
     </div>
   );
